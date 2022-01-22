4bbed738c8ea4a0feac56cbaa3fa0951e235b186
raktimhalder241 <raktimhalder241@gmail.com>
2022-01-22 16:00:26 +0000

    Added grafana script

---
+++ Grafana/scripts/grafana-server.sh
@@ -0,0 +1,7 @@
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo apt update
+sudo apt install grafana -y
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+#sudo systemctl status grafana-server
