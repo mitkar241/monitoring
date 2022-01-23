c7ec72a13cb4f551b77c1bda064346dc61a89c19
raktimhalder241 <raktimhalder241@gmail.com>
2022-01-23 07:41:54 +0000

    Added grafana script

--- Grafana/scripts/grafana-server.sh
+++ Grafana/scripts/grafana-server.sh
@@ -5,3 +5,7 @@ sudo apt install grafana -y
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server
 #sudo systemctl status grafana-server
+
+# need recent version of firefox
+sudo apt install --only-upgrade firefox
+
