081d2650d953a64ce4ef88fff45ef8ac4aabbfb7
raktimhalder241 <raktimhalder241@gmail.com>
2022-01-23 21:40:30 +0530

    Added sensuctl config view

--- sensu/scripts/sensuctl.sh
+++ sensu/scripts/sensuctl.sh
@@ -1,3 +1,4 @@
 curl -s https://packagecloud.io/install/repositories/sensu/stable/script.deb.sh | sudo bash
 sudo apt install sensu-go-cli -y
 sensuctl configure --non-interactive --url http://controller.mitkar.io:8080 --format tabular --namespace default --username raktim --password 12345678
+sensuctl config view
