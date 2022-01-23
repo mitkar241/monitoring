cf8ede7bab4cf7377ca60ba0429389deea2c3b00
raktimhalder241 <raktimhalder241@gmail.com>
2022-01-23 21:50:18 +0530

    Added deployment sensu-agent.sh

---
+++ sensu/deployment/sensu-agent.sh
@@ -0,0 +1,9 @@
+##########
+# sensu-agent
+##########
+cd
+git clone https://github.com/raktimhalder241/monitoring.git
+cd monitoring/sensu/scripts/
+sudo bash sensu-agent.sh
+cd
+rm -rf monitoring/
